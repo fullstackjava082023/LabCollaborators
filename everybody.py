ddc9643a07c9c1c6cc11c7a7abc5ae353d1ac1f0
hawedu <hawedu@kali>
2024-07-04 14:12:51 -0400

    commit from DOr

--- everybody.py
+++ everybody.py
@@ -1,7 +1,5 @@
 print("ilia: hi everybody")
 print("oshri: hi ilia")
-<<<<<<< HEAD
-=======
 print("Yoni: This is confusing.....")
->>>>>>> 63057c147f0c1c5fd51f7bbadacd43ed9a2c020c
 print("Elad: wake UP!")
+print("Hi im Dugibeat")
