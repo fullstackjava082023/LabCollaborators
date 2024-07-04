e7729211421d810c9fa7fee1efefe452bc95872b
glazedonuts616 <glazedonuts616@gmail.com>
2024-07-04 21:18:34 +0300

    yoni changes

--- everybody.py
+++ everybody.py
@@ -1,3 +1,11 @@
 print("ilia: hi everybody")
 print("oshri: hi ilia")
 print("Yoni: This is confusing.....")
+
+print("Can someone help me make a pyramid?")
+pyramid=" * "
+for i in range(1,10):
+    print(pyramid)
+    pyramid=(" "+pyramid)
+    pyramid+="**  "
+
